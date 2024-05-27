dbdb070ca8dc0a25d86d55d3f6dfe80f9867b394
kozato-jw <kozato.53103@gmail.com>
2024-05-27 23:53:04 +0900

    5/27までの作業

--- src/js/script.js
+++ src/js/script.js
@@ -2,16 +2,35 @@
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
 /* ハンバーガーメニュー */
-$(function(){
-  $('.btn-trigger').on('click', function() {
-    $(this).toggleClass('active');
-    $('.header__nav-wrapper').toggleClass('active');
+$(function() {
+  function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+  }
+
+  const css = `
+    body.hidden > *:not(header) {
+      opacity : 0;
+      transition: opacity 0.6s;
+    }
+  `;
+  addGlobalStyle(css);
+
+
+  $('.hamburger').on('click', function() {
+    $(this).toggleClass('js-active');
+    $('.header__nav-wrapper').toggleClass('js-active');
     
-    if ($(this).hasClass('active')) {
-      $('body > *:not(header)').hide();
+    if ($(this).hasClass('js-active')) {
+      $('body').addClass('hidden');
       $('body').css('overflow', 'hidden');
     } else {
-      $('body > *:not(header)').show();
+      $('body').removeClass('hidden');
       $('body').css('overflow', '');
     }
     
@@ -19,9 +38,9 @@ $(function(){
   });
 
   $('.header__nav-wrapper a').on('click', function() {
-    $('.header__nav-wrapper').removeClass('active');
-    $('.btn-trigger').removeClass('active');
-    $('body > *:not(header)').show();
+    $('.header__nav-wrapper').removeClass('js-active');
+    $('.hamburger').removeClass('js-active');
+    $('body').removeClass('hidden');
     $('body').css('overflow', '');
   });
 });
@@ -29,35 +48,47 @@ $(function(){
 
 
 /* swiperスライダー・MVセクション */
-// const mvSwiper = new Swiper('.mv_swiper', {
-//   loop: true,
-//   speed: 1500,
-//   effect: 'fade',
-//   autoplay: {
-//     delay: 2000,
-// },
-//   slidesPerView: 1,
-//   roundLengths: true,
-// });
+const mvSwiper = new Swiper('.mv_swiper', {
+  loop: true,
+  speed: 1500,
+  effect: 'fade',
+  autoplay: {
+    delay: 2000,
+},
+  slidesPerView: 1,
+  roundLengths: true,
+});
 
 /*  swiperスライダー・キャンペーンセクション */
 const campaignSwiper = new Swiper('.top-campaign__swiper', {
   loop: true,
-  slidesPerView: 1.316,
+  slidesPerView: 1.263,
   spaceBetween: 24,
   roundLengths: true,
+  speed: 600,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
   breakpoints: {
-    // 768px以上の場合
     768: {
-      slidesPerView: 3.3,
+      slidesPerView: 2.5,
+      spaceBetween: 32,
+      simulateTouch: false,
+    },
+    1024:{
+      slidesPerView: 2.8,
+      spaceBetween: 32,
+      simulateTouch: false,
+    },
+    1440: {
+      slidesPerView: 3.485,
       spaceBetween: 40,
+      simulateTouch: false,
     },
   },
 });
+
 
 
 
@@ -99,7 +130,7 @@ $('#page-top').click(function () {
 
 
 /* 画像に色幕のアニメーション(inview.js使用) */
-var box = $('.js-colorbox'),
+var box = $('.colorbox'),
     speed = 700;  
 box.each(function(){
     $(this).append('<div class="js-color"></div>')
@@ -109,7 +140,6 @@ box.each(function(){
 
     image.css('opacity','0');
     color.css('width','0%');
-    //inviewを使って背景色が画面に現れたら処理をする
     color.on('inview', function(){
         if(counter == 0){
         $(this).delay(200).animate({'width':'100%'},speed,function(){
@@ -121,8 +151,6 @@ box.each(function(){
           }
     });
 });
-
-
 
 
 });
